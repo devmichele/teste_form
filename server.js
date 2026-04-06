3f1d60e464a7235b142884b845b74f3468cb2cc7
devmichele <166833146+devmichele@users.noreply.github.com>
2026-04-06 15:47:19 -0300

    Update server.js
    alt codigo

--- server.js
+++ server.js
@@ -2,41 +2,43 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-let usuarios = [];
-app.get("/formulario", (req, res) => {
-    res.json(dados);
-});
+// =========================
+// BANCO TEMPORÁRIO (memória)
+// =========================
+let dados = [];
+
+// =========================
+// POST - SALVAR
+// =========================
 app.post("/formulario", (req, res) => {
     const { nome, email, telefone, nascimento, sexo } = req.body;
 
-    if (!nome || !email) {
-        return res.status(400).json({ erro: "Nome e email obrigatórios" });
+    if (!nome || !email || !telefone || !nascimento || !sexo) {
+        return res.status(400).json({ sucesso: false, erro: "Campos obrigatórios" });
     }
 
-    const novo = {
-        id: usuarios.length + 1,
+    dados.push({
         nome,
         email,
         telefone,
         nascimento,
         sexo
-    };
-
-    usuarios.push(novo);
-
-    console.log(novo);
+    });
 
     res.json({ sucesso: true });
 });
 
-app.get("/usuarios", (req, res) => {
-    res.json(usuarios);
+// =========================
+// GET - LISTAR
+// =========================
+app.get("/formulario", (req, res) => {
+    res.json(dados);
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("API rodando...");
+// =========================
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000");
 });
