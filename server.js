1d5da0d0722d4121d5d536d33c0f27be7a19e4be
devmichele <166833146+devmichele@users.noreply.github.com>
2026-04-06 10:52:31 -0300

    Add files via upload
    add arquivos api

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+let usuarios = [];
+
+app.post("/formulario", (req, res) => {
+    const { nome, email, telefone, nascimento, sexo } = req.body;
+
+    if (!nome || !email) {
+        return res.status(400).json({ erro: "Nome e email obrigatórios" });
+    }
+
+    const novo = {
+        id: usuarios.length + 1,
+        nome,
+        email,
+        telefone,
+        nascimento,
+        sexo
+    };
+
+    usuarios.push(novo);
+
+    console.log(novo);
+
+    res.json({ sucesso: true });
+});
+
+app.get("/usuarios", (req, res) => {
+    res.json(usuarios);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("API rodando...");
+});
